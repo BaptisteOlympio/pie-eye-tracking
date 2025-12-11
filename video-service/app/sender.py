de8f2bd13199cacda53fd3e91be6e583cb78d8b4
baptiste olympio <baptiste.olympio@gmail.com>
2025-12-11 15:32:10 +0100

    test commit windows

--- video-service/app/sender.py
+++ video-service/app/sender.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--stream_loop', action='store_true')
 parser.add_argument("-f", "--input_file", type=str, default="/workspace/data/test_gouget_cut.mp4")
+
 parser.add_argument("--port", type=int, default=8080)
 parser.add_argument("--fps", type=int, default=30)
 
