4b54540d29ec17a6d045703a75af88a313ff7db5
BaptisteOlympio <baptiste.olympio@gmail.com>
2026-01-04 17:21:04 +0100

    Création de perf pour l'affichage de vidéo service sur la webpage fini !

--- decision-service/app/routers/interface.py
+++ decision-service/app/routers/interface.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, BackgroundTasks, HTTPException
 from fastapi.responses import HTMLResponse
 from app.services import gaze
+from app.services import perf 
 from app.core import state
 
 interface_router = APIRouter()
@@ -31,6 +32,6 @@ async def get_calibration(background_tasks : BackgroundTasks) :
 
 @interface_router.get("/interface/perf")
 async def get_perf(background_tasks : BackgroundTasks) :
-    background_tasks.add_task()
+    background_tasks.add_task(perf.video_task)
     with open("app/templates/perf/index.html") as file : 
         return HTMLResponse(content=file.read())
--- decision-service/app/services/perf.py
+++ decision-service/app/services/perf.py
@@ -4,6 +4,9 @@ import asyncio
 from app.services.connection_manager import manager 
 import numpy as np
 from app.core import state
+import cv2
+
+
 
 async def video_task() :
     async with state.video_lock : 
@@ -16,6 +19,46 @@ async def video_task() :
             state.gaze_visualisation_status = state.GazeVisualisationStatus.NOT_RUNNING
 
 async def stream_video() :
+    
+    async def _recv_multipart_async(socket):
+        """Yield multipart messages from an async zmq socket."""
+        while True:
+            try:
+                parts = await socket.recv_multipart()
+            except Exception:
+                # Socket closed or interrupted
+                return
+            yield parts
+    
     context = zmq.asyncio.Context()
-    ip_video_service = ""
-    port_video_service = ""
+    ip_vs = "172.26.128.105"
+    port_vs = "8080"
+    
+    socket_video_service = context.socket(zmq.SUB)
+    socket_video_service.setsockopt(zmq.SUBSCRIBE, b"")
+    socket_video_service.setsockopt(zmq.RCVHWM, 1)
+    socket_video_service.setsockopt(zmq.LINGER, 0)
+    uri = f"tcp://{ip_vs}:{port_vs}"
+    
+    socket_video_service.connect(uri)
+    
+    async for parts in _recv_multipart_async(socket_video_service):
+        # We check that we receive 3 part : datatype, data shape and data itself
+        if not parts or len(parts) != 3:
+            continue
+        dtype_b, shape_b, data_b = parts
+        dtype = np.dtype(dtype_b.decode())
+        shape = tuple(map(int, shape_b.decode().strip("()").split(",")))
+        
+        try:
+            frame = np.frombuffer(data_b, dtype=dtype).reshape(shape)
+        except Exception:
+            # corrupted frame
+            continue
+        
+        if frame.size == 0:
+            continue
+        
+        _, jpeg = cv2.imencode(".jpg", frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+        # print("send")
+        await manager.broadcast(jpeg.tobytes(), "video")
